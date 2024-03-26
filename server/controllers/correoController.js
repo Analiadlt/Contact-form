603bbb6b0b18eefa5ba843de9358e4eb82e4579f
Analía de la Torre <87041305+Analiadlt@users.noreply.github.com>
2024-03-26 12:32:37 -0300

    controlador de correo

--- server/controllers/correoController.js
+++ server/controllers/correoController.js
@@ -1,37 +1,37 @@
 //importar express y nodeMailer como servidor de correo
-const {} = require('express');
-const nodeMailer = require('nodemailer');
+const {} = require("express");
+const nodeMailer = require("nodemailer");
 
 //definir una función para enviar el correo
-const envioCorreo = (req=request, resp=response) =>{
-    let body = req.body;
+const envioCorreo = (req = request, resp = response) => {
+  let body = req.body;
 
-    let config = nodeMailer.createTransport({
-        host: 'smtp.gmail.com',
-        post:587,
-        auth:{
-            user:'mail@innsa.com',
-            pass: ''
-        }
+  let config = nodeMailer.createTransport({
+    host: "smtp.gmail.com",
+    post: 587,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const opciones = {
+    from: "contact-form",
+    subject: body.asunto,
+    to: body.email,
+    text: body.mensaje,
+  };
+
+  config.sendMail(opciones, function (error, result) {
+    if (error) return resp.json({ ok: false, msg: error });
+
+    return resp.json({
+      ok: true,
+      msg: result,
     });
+  });
+};
 
-    const opciones = {
-        from: 'contact-form',
-        subject: body.asunto,
-        to:body.email,
-        text:body.mensaje
-    };
-
-    config.sendMail(opciones, function(error,result){
-        if (error) return resp.json({ok:false, msg:error});
-
-        return resp.json({
-            ok:true,
-            msg:result
-        })
-    })
-}
-
-module.export = {
-    envioCorreo
-}
+module.exports = {
+  envioCorreo,
+};
